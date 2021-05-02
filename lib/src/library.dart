96b1bec40ef0705d23253801bf731cf91dd40a14
Reuben Turner <groovinchip@gmail.com>
2021-05-02 13:06:24 -0400

    Address pana warning

--- lib/src/library.dart
+++ lib/src/library.dart
@@ -2,12 +2,12 @@ export 'package:flutter/cupertino.dart'
     show CupertinoColors, CupertinoDynamicColor, CupertinoIcons;
 export 'package:flutter/material.dart'
     show
-    Brightness,
-    ThemeMode,
-    Feedback,
-    DefaultMaterialLocalizations,
-    PageTransitionsBuilder,
-    FlutterLogo,
-    CircleAvatar;
+        Brightness,
+        ThemeMode,
+        Feedback,
+        DefaultMaterialLocalizations,
+        PageTransitionsBuilder,
+        FlutterLogo,
+        CircleAvatar;
 
 export 'package:flutter/widgets.dart';
