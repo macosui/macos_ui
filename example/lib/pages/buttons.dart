25cf2dc22af7810a60df26862d0566f4d4ea14a6
Reuben Turner <groovinchip@gmail.com>
2021-05-27 19:07:19 -0400

    showcase_app (#121)
    * Start working on showcase
    
    * More showcase work
    
    * Wrap up refactor
    
    * Fix clicks
    
    * Fix textfield

---
+++ example/lib/pages/buttons.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:macos_ui/macos_ui.dart';
+import 'package:flutter/widgets.dart';
+
+class ButtonsPage extends StatefulWidget {
+  @override
+  _ButtonsPageState createState() => _ButtonsPageState();
+}
+
+class _ButtonsPageState extends State<ButtonsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Text('MacosBackButton'),
+          const SizedBox(height: 8),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MacosBackButton(
+                onPressed: () => print('click'),
+                fillColor: Colors.transparent,
+              ),
+              const SizedBox(width: 16.0),
+              MacosBackButton(
+                onPressed: () => print('click'),
+                //fillColor: Colors.transparent,
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Text('MacosIconButton'),
+          const SizedBox(height: 8),
+          MacosIconButton(
+            icon: Icon(
+              CupertinoIcons.star_fill,
+              color: Colors.white,
+            ),
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(7),
+            onPressed: () {},
+          ),
+          const SizedBox(height: 20),
+          PushButton(
+            buttonSize: ButtonSize.large,
+            child: Text('large PushButton'),
+            onPressed: () {},
+          ),
+          const SizedBox(height: 20),
+          PushButton(
+            buttonSize: ButtonSize.small,
+            child: Text('small PushButton'),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ example/lib/pages/indicators.dart
@@ -0,0 +1,48 @@
+import 'package:macos_ui/macos_ui.dart';
+import 'package:flutter/widgets.dart';
+
+class IndicatorsPage extends StatefulWidget {
+  @override
+  _IndicatorsPageState createState() => _IndicatorsPageState();
+}
+
+class _IndicatorsPageState extends State<IndicatorsPage> {
+  double ratingValue = 0;
+  double sliderValue = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          CapacityIndicator(
+            value: sliderValue,
+            onChanged: (v) => setState(() => sliderValue = v),
+            discrete: true,
+          ),
+          SizedBox(height: 20),
+          CapacityIndicator(
+            value: sliderValue,
+            onChanged: (v) => setState(() => sliderValue = v),
+          ),
+          SizedBox(height: 20),
+          RatingIndicator(
+            value: ratingValue,
+            onChanged: (v) => setState(() => ratingValue = v),
+          ),
+          SizedBox(height: 20),
+          ProgressCircle(),
+          SizedBox(height: 20),
+          RelevanceIndicator(
+            value: 25,
+            amount: 50,
+          ),
+          SizedBox(height: 20),
+          ProgressCircle(
+            value: 50,
+          ),
+        ],
+      ),
+    );
+  }
+}
