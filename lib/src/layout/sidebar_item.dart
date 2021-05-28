60afdb35db040738e04869d6cbc814aaa5810c5c
GroovinChip <groovinchip@gmail.com>
2021-05-28 13:55:41 -0400

    Add docs

--- lib/src/layout/sidebar_item.dart
+++ lib/src/layout/sidebar_item.dart
@@ -2,9 +2,9 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:macos_ui/src/library.dart';
 
-///
+/// A macOS style navigation-list item intended for use in a [Sidebar]
 class SidebarItem extends StatefulWidget {
-  ///
+  /// Builds a [SidebarItem].
   const SidebarItem({
     Key? key,
     required this.leading,
@@ -14,16 +14,24 @@ class SidebarItem extends StatefulWidget {
     this.semanticsLabel,
   }) : super(key: key);
 
-  ///
+  /// The widget to lay out first. 
+  /// 
+  /// Typically an [Icon] widget.
   final Widget? leading;
 
-  ///
+  /// Indicates what content this widget represents.
+  /// 
+  /// Typically a [Text] widget.
   final Widget label;
 
-  ///
+  /// A function to perform when the widget is clicked or tapped.
+  /// 
+  /// Typically a [Navigator] call
   final VoidCallback? onClick;
 
-  ///
+  /// The color to paint this widget as when focused.
+  /// 
+  /// Defaults to [CupertinoColors.systemBlue].
   final Color focusColor;
 
   /// The semantic label used by screen readers.
