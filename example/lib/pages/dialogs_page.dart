cd08ab42e44dc9d6a98afcf8acbe463dd7afe24c
Reuben Turner <groovinchip@gmail.com>
2021-06-09 19:38:03 -0400

    Latest work from dev (#136)
    * Fix some errors
    
    * Fix disclosure item color (#129)
    
    * Update sidebar_item.dart
    
    * Update sidebar_item.dart
    
    * Sidebar bottom item (#131)
    
    * Add option for Fixed Bottom Widget in Sidebar
    
    * Add Bottom Item to Example
    
    * Update Changelog
    
    * Formatting
    
    * Alert dialog 2 (#132)
    
    * Initial work on AlertDialog
    
    * Add some docs, tweak sizing for appIcon
    
    * message padding
    
    * Looking like a proper dialog now
    
    * Fix padding for dialog with no secondary action. Add more examples.
    
    * Bump version, CHANGELOG.md
    
    * Merge latest dev
    
    * Add MacosAlertDialog to README.md
    
    * Fix name, link
    
    * Address PR feedback
    
    * Tweak sidebar bottom a little
    
    * Suppress option for `MacosAlertDialog` (#135)
    
    * Initial work on AlertDialog
    
    * Add some docs, tweak sizing for appIcon
    
    * message padding
    
    * Looking like a proper dialog now
    
    * Fix padding for dialog with no secondary action. Add more examples.
    
    * Bump version, CHANGELOG.md
    
    * Merge latest dev
    
    * Add MacosAlertDialog to README.md
    
    * Fix name, link
    
    * Address PR feedback
    
    * Add suppress option to MacosAlertDialog
    
    * Adjust dialog text
    
    * Address PR feedback
    
    Co-authored-by: Bruno D'Luka <45696119+bdlukaa@users.noreply.github.com>
    Co-authored-by: Anton Borries <anton.borries@gmail.com>

--- example/lib/pages/dialogs_page.dart
+++ example/lib/pages/dialogs_page.dart
@@ -81,7 +81,7 @@ class _DialogsPageState extends State<DialogsPage> {
         const SizedBox(height: 16),
         PushButton(
           buttonSize: ButtonSize.large,
-          child: Text('Show Alert Dialog 2'),
+          child: Text('Show Alert Dialog 3'),
           onPressed: () => showDialog(
             context: context,
             builder: (_) => MacosAlertDialog(
@@ -119,6 +119,79 @@ class _DialogsPageState extends State<DialogsPage> {
             ),
           ),
         ),
+        const SizedBox(height: 16),
+        PushButton(
+          buttonSize: ButtonSize.large,
+          child: Text('Show Alert Dialog 4'),
+          onPressed: () => showDialog(
+            context: context,
+            builder: (_) => MacosAlertDialog(
+              appIcon: FlutterLogo(
+                size: 56,
+              ),
+              title: Text(
+                'Alert Dialog with Secondary Action',
+              ),
+              message: Text(
+                'This is an alert dialog with primary action and secondary '
+                'action laid out vertically. It also contains a "suppress" option.',
+                textAlign: TextAlign.center,
+              ),
+              horizontalActions: false,
+              primaryButton: PushButton(
+                buttonSize: ButtonSize.large,
+                child: Text('Primary'),
+                onPressed: () {},
+              ),
+              secondaryButton: PushButton(
+                buttonSize: ButtonSize.large,
+                color: MacosDynamicColor.resolve(
+                  MacosColors.controlColor,
+                  context,
+                ),
+                child: Text(
+                  'Secondary',
+                  style: TextStyle(
+                    color: MacosDynamicColor.resolve(
+                      MacosColors.controlColor,
+                      context,
+                    ),
+                  ),
+                ),
+                onPressed: () {},
+              ),
+              suppress: DoNotNotifyRow(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class DoNotNotifyRow extends StatefulWidget {
+  const DoNotNotifyRow({Key? key}) : super(key: key);
+
+  @override
+  _DoNotNotifyRowState createState() => _DoNotNotifyRowState();
+}
+
+class _DoNotNotifyRowState extends State<DoNotNotifyRow> {
+  bool suppress = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        MacosCheckbox(
+          value: suppress,
+          onChanged: (value) {
+            setState(() => suppress = value);
+          },
+        ),
+        const SizedBox(width: 8),
+        Text('Don\'t ask again'),
       ],
     );
   }
