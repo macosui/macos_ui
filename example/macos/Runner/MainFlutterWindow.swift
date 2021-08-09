7d1df78705c8089e31948ce7161677e1625ec5d3
Leslie Arkorful <lesliearkorful@gmail.com>
2021-08-09 01:11:26 +0000

    Update MainFlutterWindow.swift

--- example/macos/Runner/MainFlutterWindow.swift
+++ example/macos/Runner/MainFlutterWindow.swift
@@ -8,9 +8,18 @@ class MainFlutterWindow: NSWindow {
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
 
-    self.titlebarAppearsTransparent = true
-    self.isMovableByWindowBackground = true
+    if #available(macOS 10.13, *) {
+      let customToolbar = NSToolbar()
+      customToolbar.showsBaselineSeparator = false
+      self.toolbar = customToolbar
+    }
     self.titleVisibility = .hidden
+    self.titlebarAppearsTransparent = true
+    if #available(macOS 11.0, *) {
+      self.toolbarStyle = .unified
+    }
+
+    self.isMovableByWindowBackground = true
     self.styleMask.insert(NSWindow.StyleMask.fullSizeContentView)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
