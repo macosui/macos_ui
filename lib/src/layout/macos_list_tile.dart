94308277d7a5c2dcbd258ed2b85ef9b47d4aa87d
groovinchip <groovinchip@gmail.com>
2021-10-12 15:52:47 -0400

    feat(package): add mouseCursor property

--- lib/src/layout/macos_list_tile.dart
+++ lib/src/layout/macos_list_tile.dart
@@ -13,6 +13,7 @@ class MacosListTile extends StatelessWidget {
     this.leadingWhitespace = 8,
     this.onClick,
     this.onLongPress,
+    this.mouseCursor = MouseCursor.defer,
   }) : super(key: key);
 
   /// A widget to display before the [title].
@@ -35,37 +36,44 @@ class MacosListTile extends StatelessWidget {
   /// A callback to perform when the widget is long-pressed.
   final VoidCallback? onLongPress;
 
+  /// The [MouseCursor] to use for this widget.
+  final MouseCursor mouseCursor;
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onClick,
-      onLongPress: onLongPress,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          if (leading != null) leading!,
-          SizedBox(width: leadingWhitespace),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              DefaultTextStyle(
-                style: MacosTheme.of(context).typography.headline.copyWith(
-                      fontWeight: FontWeight.bold,
-                    ),
-                child: title,
-              ),
-              if (subtitle != null)
+    return MouseRegion(
+      cursor: mouseCursor,
+
+      child: GestureDetector(
+        onTap: onClick,
+        onLongPress: onLongPress,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (leading != null) leading!,
+            SizedBox(width: leadingWhitespace),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
                 DefaultTextStyle(
-                  style: MacosTheme.of(context).typography.subheadline.copyWith(
-                        color: MacosTheme.brightnessOf(context).isDark
-                            ? MacosColors.systemGrayColor
-                            : const MacosColor(0xff88888C),
+                  style: MacosTheme.of(context).typography.headline.copyWith(
+                        fontWeight: FontWeight.bold,
                       ),
-                  child: subtitle!,
+                  child: title,
                 ),
-            ],
-          ),
-        ],
+                if (subtitle != null)
+                  DefaultTextStyle(
+                    style: MacosTheme.of(context).typography.subheadline.copyWith(
+                          color: MacosTheme.brightnessOf(context).isDark
+                              ? MacosColors.systemGrayColor
+                              : const MacosColor(0xff88888C),
+                        ),
+                    child: subtitle!,
+                  ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
