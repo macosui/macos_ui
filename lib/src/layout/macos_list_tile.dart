2b350eca81b6ccd6c97d59426c070da075507095
groovinchip <groovinchip@gmail.com>
2021-10-12 15:21:56 -0400

    feat: add leadingWhitespace prop to MacosListTile and add DefaultTextStyles to title and subtitle

--- lib/src/layout/macos_list_tile.dart
+++ lib/src/layout/macos_list_tile.dart
@@ -1,3 +1,4 @@
+import 'package:macos_ui/macos_ui.dart';
 import 'package:macos_ui/src/library.dart';
 
 /// A widget that aims to approximate the [ListTile] widget found in
@@ -9,6 +10,7 @@ class MacosListTile extends StatelessWidget {
     this.leading,
     required this.title,
     this.subtitle,
+    this.leadingWhitespace = 8,
   }) : super(key: key);
 
   /// A widget to display before the [title].
@@ -20,18 +22,36 @@ class MacosListTile extends StatelessWidget {
   /// Additional content displayed below the [title].
   final Widget? subtitle;
 
+  /// The amount of whitespace between the [leading] and [title] widgets.
+  ///
+  /// Defaults to `8`.
+  final double? leadingWhitespace;
+
   @override
   Widget build(BuildContext context) {
     return Row(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         if (leading != null) leading!,
-        const SizedBox(width: 8),
+        SizedBox(width: leadingWhitespace),
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            title,
-            if (subtitle != null) subtitle!,
+            DefaultTextStyle(
+              style: MacosTheme.of(context).typography.headline.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+              child: title,
+            ),
+            if (subtitle != null)
+              DefaultTextStyle(
+                style: MacosTheme.of(context).typography.subheadline.copyWith(
+                      color: MacosTheme.brightnessOf(context).isDark
+                          ? MacosColors.systemGrayColor
+                          : const MacosColor(0xff88888C),
+                    ),
+                child: subtitle!,
+              ),
           ],
         ),
       ],
