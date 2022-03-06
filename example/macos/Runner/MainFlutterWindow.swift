a9acdce0ec90147f8f5f86ac0b867fd5dd349fba
mj-shifu <77107165+mj-shifu@users.noreply.github.com>
2022-03-06 18:44:22 +0100

    Sidebar transparency effect (#175)
    * Transparency sidebar effect
    
    Added a transparency effect to the sidebar
    
    * fix: Formatted window.dart
    
    * chore: update changelog, version
    
    * fix: removed unused imports
    
    * fix: applied enhancement from @jmatth
    
    this fixes weird behavior on windows and web
    
    * fix: double null check
    
    widget.sidebar.decoration.color was checked twice
    (according to suggestion by @jmatth)

--- example/macos/Runner/MainFlutterWindow.swift
+++ example/macos/Runner/MainFlutterWindow.swift
@@ -22,6 +22,20 @@ class MainFlutterWindow: NSWindow {
     self.isMovableByWindowBackground = true
     self.styleMask.insert(NSWindow.StyleMask.fullSizeContentView)
 
+    self.isOpaque = false
+    self.backgroundColor = .clear
+    let contentView = contentViewController!.view;
+    let superView = contentView.superview!;
+    let blurView = NSVisualEffectView()
+    blurView.frame = superView.bounds
+    blurView.autoresizingMask = [.width, .height]
+    blurView.blendingMode = NSVisualEffectView.BlendingMode.behindWindow
+    if #available(macOS 10.14, *) {
+      blurView.material = .underWindowBackground
+    }
+    superView.replaceSubview(contentView, with: blurView)
+    blurView.addSubview(contentView)
+
     RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
