0c46c497033b947155caaf40396ccde5bff95256
GroovinChip <groovinchip@gmail.com>
2022-05-02 12:12:19 -0400

    chore: improve prelaunch script

--- pr_prelaunch_tasks.sh
+++ pr_prelaunch_tasks.sh
@@ -1,2 +1,35 @@
-flutter format lib && flutter format test
-flutter test
+flutter format lib --set-exit-if-changed
+if [ $? -eq 1 ]; then
+  flutter format lib
+  git add .
+  git commit -m "chore: run flutter format lib"
+fi
+flutter format test --set-exit-if-changed
+if [ $? -eq 1 ]; then
+  flutter format test
+  git add .
+  git commit -m "chore: run flutter format test"
+fi
+echo "Run dart fix --dry-run? [y/n]"
+read dryRunResponse
+if [ "$dryRunResponse" = "y" ]; then
+  dart fix --dry-run
+fi
+echo "Run dart fix --apply? [y/n]"
+read applyResponse
+if [ "$applyResponse" = "y" ]; then
+  dart fix --apply
+  if [ -z "$(git status --porcelain)" ]; then
+    echo "No changes to commit"
+  else
+    git add .
+    git commit -m "chore: run dart fix --apply"
+  fi
+fi
+echo "Run tests? [y/n]"
+read testResponse
+if [ "$applyResponse" = "y" ]; then
+  flutter test
+else
+  exit 0
+fi
