630bccccdd54af1431f65e14ea0762f640ee75cd
GroovinChip <groovinchip@gmail.com>
2022-05-05 10:46:14 -0400

    test: fix failing date picker test

--- test/selectors/date_picker_test.dart
+++ test/selectors/date_picker_test.dart
@@ -5,7 +5,7 @@ import 'package:macos_ui/macos_ui.dart';
 
 void main() {
   group('MacosDatePicker tests', () {
-    testWidgets('MacosDatePicker renders the expected date', (tester) async {
+    testWidgets('textual MacosDatePicker renders the expected date', (tester) async {
       final today = DateTime.now();
       await tester.pumpWidget(
         MacosApp(
@@ -17,6 +17,7 @@ void main() {
                     return Center(
                       child: MacosDatePicker(
                         onDateChanged: (date) {},
+                        style: DatePickerStyle.textual,
                       ),
                     );
                   },
