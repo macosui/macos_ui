a5f5f2c73df23c9dabf78a6bd2d18569bfed5012
GroovinChip <groovinchip@gmail.com>
2022-05-12 14:32:42 -0400

    chore: remove unused import

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,7 +6,6 @@ import 'package:example/pages/indicators_page.dart';
 import 'package:example/pages/selectors_page.dart';
 import 'package:example/pages/toolbar_page.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:macos_ui/macos_ui.dart';
 import 'package:provider/provider.dart';
 
