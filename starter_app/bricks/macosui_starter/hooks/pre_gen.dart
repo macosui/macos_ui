0a9e53d5cc7e9ded5231dc0d34d479cb897d70c8
GroovinChip <groovinchip@gmail.com>
2022-05-13 13:50:08 -0400

    chore: run flutter format

--- starter_app/bricks/macosui_starter/hooks/pre_gen.dart
+++ starter_app/bricks/macosui_starter/hooks/pre_gen.dart
@@ -4,7 +4,7 @@ void run(HookContext context) {
   if (context.vars['use_translucency'] == true) {
     context.vars['hide_native_title_bar'] = true;
   } else {
-    context.vars['hide_native_title_bar'] =
-        context.logger.confirm('${styleBold.wrap(lightGreen.wrap('?'))} Hide native window title bar?');
+    context.vars['hide_native_title_bar'] = context.logger.confirm(
+        '${styleBold.wrap(lightGreen.wrap('?'))} Hide native window title bar?');
   }
 }
