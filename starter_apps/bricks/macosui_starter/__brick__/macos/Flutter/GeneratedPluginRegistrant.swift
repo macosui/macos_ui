15c81365f53c0d6ee75ec8aaf0f8d22f51178721
GroovinChip <groovinchip@gmail.com>
2022-05-13 10:56:42 -0400

    feat: first pass at starter app brick

---
+++ starter_apps/bricks/macosui_starter/__brick__/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import macos_ui
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  MacOSUiPlugin.register(with: registry.registrar(forPlugin: "MacOSUiPlugin"))
+}
