fdba08d0535fa3ad332c2ddd244c33db46208a12
GroovinChip <groovinchip@gmail.com>
2022-07-04 17:12:15 -0400

    chore: fixup scripts

--- publish_tasks.sh
+++ publish_tasks.sh
@@ -1,14 +1,14 @@
 # MAINTAINER ONLY SCRIPT. DO NOT RUN THIS SCRIPT UNLESS YOU ARE THE MAINTAINER.
 pana --no-warning
 echo "Are you ready to dry-run publish macos_ui? [y/n]"
-read dryRunResponse
+read -r dryRunResponse
 if [ "$dryRunResponse" = "y" ]; then
   flutter pub publish --dry-run
 else
   exit 0
 fi
 echo "Are you ready to publish macos_ui to pub.dev? [y/n]"
-read publishResponse
+read -r publishResponse
 if [ "$publishResponse" = "y" ]; then
   flutter pub publish
 else
