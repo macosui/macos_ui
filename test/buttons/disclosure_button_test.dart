2fa99025f8b21cf56605c77047adab296b761680
Umar Salim <umar.salim@niyotail.com>
2023-02-03 23:41:57 +0530

    remove scroll controller from content area in tests

--- test/buttons/disclosure_button_test.dart
+++ test/buttons/disclosure_button_test.dart
@@ -18,7 +18,7 @@ void main() {
           child: MacosScaffold(
             children: [
               ContentArea(
-                builder: (context, scrollController) {
+                builder: (context) {
                   return MacosDisclosureButton(
                     onPressed: mockOnPressedFunction.handler,
                   );
