7f1f10d7187f843928c57fc18c2c0b73da51690d
Adrian Samoticha <adrian@samoticha.de>
2023-02-20 20:45:18 +0100

    doc: document global wallpaper tinting settings

--- lib/src/layout/wallpaper_tinting_settings/global_wallpaper_tinting_settings.dart
+++ lib/src/layout/wallpaper_tinting_settings/global_wallpaper_tinting_settings.dart
@@ -2,22 +2,30 @@ import 'dart:async';
 
 import 'package:macos_ui/src/layout/wallpaper_tinting_settings/wallpaper_tinting_settings_data.dart';
 
-/// TODO: document this
+/// A class that provides a global instance of [WallpaperTintingSettingsData].
 class GlobalWallpaperTintingSettings {
+  /// The [WallpaperTintingSettingsData] instance.
   static WallpaperTintingSettingsData data = WallpaperTintingSettingsData();
+
+  /// The [StreamController] for an event stream that is triggered when [data]
+  /// changes.
   static final _onDataChangedStreamController =
       StreamController<WallpaperTintingSettingsData>.broadcast();
 
+  /// A stream that can be used to listen to [data] changes.
   static Stream<WallpaperTintingSettingsData> get onDataChangedStream =>
       _onDataChangedStreamController.stream;
 
+  /// Gets whether wallpaper tinting should be enabled.
   static bool get isWallpaperTintingEnabled => data.isWallpaperTintingEnabled;
 
+  /// Increments the number of active overrides.
   static void addWallpaperTintingOverride() {
     data.addOverride();
     _onDataChangedStreamController.add(data);
   }
 
+  /// Decrements the number of active overrides.
   static void removeWallpaperTintingOverride() {
     data.removeOverride();
     _onDataChangedStreamController.add(data);
