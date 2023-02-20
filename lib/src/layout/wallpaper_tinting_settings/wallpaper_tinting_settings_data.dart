4986d9e82c3633fcc63823c4ee8d85edf91faae6
Adrian Samoticha <adrian@samoticha.de>
2023-02-20 20:40:04 +0100

    doc: document wallpaper tinting settings data

--- lib/src/layout/wallpaper_tinting_settings/wallpaper_tinting_settings_data.dart
+++ lib/src/layout/wallpaper_tinting_settings/wallpaper_tinting_settings_data.dart
@@ -1,11 +1,17 @@
-/// TODO: document this
+/// Holds data related to wallpaper tinting.
 class WallpaperTintingSettingsData {
+  /// The number of wallpaper tinting overrides that are currently active.
+  ///
+  /// A wallpaper tinting override causes wallpaper tinting to be disabled.
   int numberOfWallpaperTintingOverrides = 0;
 
+  /// Gets whether wallpaper tinting should be enabled.
   bool get isWallpaperTintingEnabled => numberOfWallpaperTintingOverrides == 0;
 
+  /// Increment the number of active overrides.
   void addOverride() => numberOfWallpaperTintingOverrides += 1;
 
+  /// Decrements the number of active overrides.
   void removeOverride() {
     numberOfWallpaperTintingOverrides -= 1;
     assert(numberOfWallpaperTintingOverrides >= 0);
