ae60f3162181b3c17099dbbb264c41072b111d6e
Adrian Samoticha <adrian@samoticha.de>
2023-02-20 20:40:39 +0100

    change: make `numberOfWallpaperTintingOverrides` private

--- lib/src/layout/wallpaper_tinting_settings/wallpaper_tinting_settings_data.dart
+++ lib/src/layout/wallpaper_tinting_settings/wallpaper_tinting_settings_data.dart
@@ -3,17 +3,17 @@ class WallpaperTintingSettingsData {
   /// The number of wallpaper tinting overrides that are currently active.
   ///
   /// A wallpaper tinting override causes wallpaper tinting to be disabled.
-  int numberOfWallpaperTintingOverrides = 0;
+  int _numberOfWallpaperTintingOverrides = 0;
 
   /// Gets whether wallpaper tinting should be enabled.
-  bool get isWallpaperTintingEnabled => numberOfWallpaperTintingOverrides == 0;
+  bool get isWallpaperTintingEnabled => _numberOfWallpaperTintingOverrides == 0;
 
   /// Increment the number of active overrides.
-  void addOverride() => numberOfWallpaperTintingOverrides += 1;
+  void addOverride() => _numberOfWallpaperTintingOverrides += 1;
 
   /// Decrements the number of active overrides.
   void removeOverride() {
-    numberOfWallpaperTintingOverrides -= 1;
-    assert(numberOfWallpaperTintingOverrides >= 0);
+    _numberOfWallpaperTintingOverrides -= 1;
+    assert(_numberOfWallpaperTintingOverrides >= 0);
   }
 }
