1f4119afb256a508550543b047d503f5667390da
Adrian Samoticha <adrian@samoticha.de>
2023-02-21 14:24:04 +0100

    refactor: refactor wallpaper tinted area

--- lib/src/layout/wallpaper_tinted_area.dart
+++ lib/src/layout/wallpaper_tinted_area.dart
@@ -23,6 +23,30 @@ class WallpaperTintedArea extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return _WallpaperTintedAreaLayoutBuilder(
+      backgroundColor: backgroundColor,
+      child: child,
+    );
+  }
+}
+
+class _WallpaperTintedAreaLayoutBuilder extends StatelessWidget {
+  const _WallpaperTintedAreaLayoutBuilder({
+    required this.backgroundColor,
+    required this.child,
+  });
+
+  /// The color to apply to the background when wallpaper tinting is disabled.
+  final Color backgroundColor;
+
+  /// The widget below this widget in the tree.
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    // This LayoutBuilder forces the widget to be rebuilt when a layout change
+    // is detected. This is necessary for the VisualEffectSubviewContainer to
+    // be updated.
     return LayoutBuilder(
       builder: (context, _) {
         return VisualEffectSubviewContainer(
@@ -31,32 +55,58 @@ class WallpaperTintedArea extends StatelessWidget {
             builder: (context, data) {
               final isWallpaperTintingEnabled = data.isWallpaperTintingEnabled;
 
-              return TweenAnimationBuilder<double>(
-                duration: const Duration(milliseconds: 100),
-                tween: Tween<double>(
-                  begin: isWallpaperTintingEnabled ? 0.0 : 1.0,
-                  end: isWallpaperTintingEnabled ? 0.0 : 1.0,
-                ),
-                builder: (context, value, child) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      color: backgroundColor.withOpacity(value),
-                      backgroundBlendMode: BlendMode.src,
-                    ),
-                    child: child,
-                  );
-                },
-                child: Opacity(
-                  // For some reason, omitting this Opacity widget causes
-                  // a dark background to appear.
-                  opacity: 1.0,
-                  child: child,
-                ),
+              return _WallpaperTintedAreaTweenAnimationBuilder(
+                isWallpaperTintingEnabled: isWallpaperTintingEnabled,
+                backgroundColor: backgroundColor,
+                child: child,
               );
             },
           ),
         );
       },
+    );
+  }
+}
+
+class _WallpaperTintedAreaTweenAnimationBuilder extends StatelessWidget {
+  const _WallpaperTintedAreaTweenAnimationBuilder({
+    required this.isWallpaperTintingEnabled,
+    required this.backgroundColor,
+    required this.child,
+  });
+
+  /// Whether wallpaper tinting is enabled.
+  final bool isWallpaperTintingEnabled;
+
+  /// The color to apply to the background when wallpaper tinting is disabled.
+  final Color backgroundColor;
+
+  /// The widget below this widget in the tree.
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder<double>(
+      duration: const Duration(milliseconds: 100),
+      tween: Tween<double>(
+        begin: isWallpaperTintingEnabled ? 0.0 : 1.0,
+        end: isWallpaperTintingEnabled ? 0.0 : 1.0,
+      ),
+      builder: (context, value, child) {
+        return Container(
+          decoration: BoxDecoration(
+            color: backgroundColor.withOpacity(value),
+            backgroundBlendMode: BlendMode.src,
+          ),
+          child: child,
+        );
+      },
+      child: Opacity(
+        // For some reason, omitting this Opacity widget causes
+        // a dark background to appear.
+        opacity: 1.0,
+        child: child,
+      ),
     );
   }
 }
