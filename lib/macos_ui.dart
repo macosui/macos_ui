111e83b414350beb7e6c5c1fddbc21858e5729e6
Adrian Samoticha <adrian@samoticha.de>
2023-02-21 14:32:13 +0100

    change: export wallpaper tinted area

--- lib/macos_ui.dart
+++ lib/macos_ui.dart
@@ -81,5 +81,6 @@ export 'src/theme/search_field_theme.dart';
 export 'src/theme/time_picker_theme.dart';
 export 'src/theme/tooltip_theme.dart';
 export 'src/theme/typography.dart';
+export 'src/layout/wallpaper_tinted_area.dart';
 
 export 'package:macos_window_utils/macos_window_utils.dart';
