08fca1c548b807af99eea06c8c34989fad26909a
GroovinChip <groovinchip@gmail.com>
2023-02-26 13:25:26 -0500

    small tweak to windowBackgroundColor

--- lib/src/theme/macos_colors.dart
+++ lib/src/theme/macos_colors.dart
@@ -286,7 +286,7 @@ class MacosColors {
   /// The color to use for the window background.
   static const windowBackgroundColor = CupertinoDynamicColor.withBrightness(
     color: MacosColor.fromRGBO(238, 236, 236, 1.0),
-    darkColor: MacosColor.fromRGBO(32, 30, 31, 1.0),
+    darkColor: MacosColor.fromRGBO(31, 29, 31, 1.0),
   );
 
   /// The background behind a document's content.
