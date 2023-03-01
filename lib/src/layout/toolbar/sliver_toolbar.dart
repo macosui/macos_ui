904964385ed6d68c21f9af206e2ac7cfc70dc626
Adrian Samoticha <adrian@samoticha.de>
2023-03-01 14:47:03 +0100

    docs: document sliver toolbar's `isVisible` property

--- lib/src/layout/toolbar/sliver_toolbar.dart
+++ lib/src/layout/toolbar/sliver_toolbar.dart
@@ -139,7 +139,24 @@ class SliverToolBar extends StatefulWidget with Diagnosticable {
   /// Defaults to `0.9`.
   final double toolbarOpacity;
 
-  /// TODO: document this
+  /// Whether this [SliverToolBar] is currently visible on the screen (that is,
+  /// not e.g. hidden by an [IndexedStack]).
+  ///
+  /// By default, macos_ui applies wallpaper tinting to the application's
+  /// window to match macOS' native appearance:
+  ///
+  /// <img src="https://user-images.githubusercontent.com/86920182/220182724-d78319d7-5c41-4e8c-b785-a73a6ea24927.jpg" width=640/>
+  ///
+  /// However, this effect is realized by inserting `NSVisualEffectView`s behind
+  /// Flutter's canvas and turning the background of areas that are meant to be
+  /// affected by wallpaper tinting transparent. Since Flutter's
+  /// [`ImageFilter.blur`](https://api.flutter.dev/flutter/dart-ui/ImageFilter/ImageFilter.blur.html)
+  /// does not support transparency, wallpaper tinting is disabled automatically
+  /// when this widget's [isVisible] is true.
+  ///
+  /// This is meant to be a temporary solution until
+  /// [#16296](https://github.com/flutter/flutter/issues/16296) is resolved in
+  /// the Flutter project.
   final bool isVisible;
 
   @override
