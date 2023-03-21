c99cb0a7f72036d7da5ed645d57d04d037fb40fb
Reuben Turner <groovinchip@gmail.com>
2023-03-21 10:41:57 -0400

    DCM lint updates and related fixes (#393)
    * lints: add `double-literal-format` lint and fixes
    
    * lints: add `prefer-first`, `prefer-last`, and `prefer-immediate-return`
    
    Includes fixes
    
    * chore: remove a redundant `async`
    
    * fix: avoid non-const or final global state
    
    * lints: add always-remove-listener and fix
    
    * lints: add `avoid-unnecessary-setstate` and fixes
    
    * lints: add `avoid-wrapping-in-padding` and fixes
    
    * lints: add `prefer-const-border-radius` and fixes
    
    * lints: add `prefer-correct-edge-insets-constructor` and fixes
    
    * lints: add `use-setstate-synchronously` and fixes
    
    * lints: remove `number-of-parameters`
    
    * fix(MacosTextField): remove FocusNode listener instead of disposing FocusNode
    
    * chore(actions): split out tests into their own workflow

--- lib/src/fields/text_field.dart
+++ lib/src/fields/text_field.dart
@@ -358,7 +358,7 @@ class MacosTextField extends StatefulWidget {
     this.focusNode,
     this.decoration,
     this.focusedDecoration,
-    this.padding = const EdgeInsets.fromLTRB(2.0, 4.0, 2.0, 4.0),
+    this.padding = const EdgeInsets.symmetric(horizontal: 2.0, vertical: 4.0),
     this.placeholder,
     this.placeholderStyle = _kDefaultPlaceholderStyle,
     this.prefix,
@@ -1157,6 +1157,7 @@ class _MacosTextFieldState extends State<MacosTextField>
   void dispose() {
     _focusNode?.dispose();
     _controller?.dispose();
+    _effectiveFocusNode.removeListener(_handleFocusChanged);
     super.dispose();
   }
 
