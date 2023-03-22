0a89be89089e1be7e09568a47b99febcd3bc5f40
Adrian Samoticha <adrian@samoticha.de>
2023-03-22 17:13:09 +0100

    upgrade to macos_window_utils 1.1.2

--- example/macos/Runner/MainFlutterWindow.swift
+++ example/macos/Runner/MainFlutterWindow.swift
@@ -1,18 +1,14 @@
 import Cocoa
 import FlutterMacOS
-import macos_window_utils
 
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
+    let flutterViewController = FlutterViewController.init()
     let windowFrame = self.frame
-    let macOSWindowUtilsViewController = MacOSWindowUtilsViewController()
-    self.contentViewController = macOSWindowUtilsViewController
+    self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
 
-    /* Initialize the macos_window_utils plugin */
-    MainFlutterWindowManipulator.start(mainFlutterWindow: self)
-
-    RegisterGeneratedPlugins(registry: macOSWindowUtilsViewController.flutterViewController)
+    RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
   }
