456f849ec39d2b55f63846b40c4e0b3ad3e00c67
st merlhin <77164238+stMerlHin@users.noreply.github.com>
2023-07-11 07:18:55 +0000

    Update example/lib/pages/buttons_page.dart
    Co-authored-by: Reuben Turner <groovinchip@gmail.com>

--- example/lib/pages/buttons_page.dart
+++ example/lib/pages/buttons_page.dart
@@ -66,7 +66,7 @@ class _ButtonsPageState extends State<ButtonsPage> {
             resizableSide: ResizableSide.right,
             child: const Center(
                 child: Text('Resizable Pane')
-            )
+            ),
         ),
         ContentArea(
           builder: (context, scrollController) {
