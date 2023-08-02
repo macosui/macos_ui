1a898437e4316f292d4803548a49998848c8af67
Kate Lovett <katelovett@google.com>
2023-08-02 15:12:26 -0500

    Fix compilation errors

--- test/indicators/capacity_indicators_test.dart
+++ test/indicators/capacity_indicators_test.dart
@@ -5,6 +5,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:macos_ui/macos_ui.dart';
 
+// TODO(): Remove once mock_canvas in flutter_test reaches stable.
 import '../mock_canvas.dart';
 
 void main() {
