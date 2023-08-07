aa8f90d1a708ea0710e2a27ac42b2bacffede59a
Adrian Samoticha <adrian@samoticha.de>
2023-08-07 14:42:27 +0200

    do not init `AccentColorListener` if platform isn’t macOS

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -202,6 +202,9 @@ class AccentColorListener {
 
   /// Initializes this class.
   void _init() {
+    if (kIsWeb) return;
+    if (!Platform.isMacOS) return;
+
     _initCurrentAccentColor();
     _initSystemColorObserver();
   }
